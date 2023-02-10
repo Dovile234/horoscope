505c23156f4745fe28c693df9479ebc4fdd2f9f2
Mona Lisa <zdovile123@gmail.com>
2023-02-10 16:13:40 +0200

    changed api

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ btn.addEventListener("click", (event) => {
   event.preventDefault();
   let signsSelectValue = signsSelect.value;
   let daySelectValue = daySelect.value;
+  const options = {
+    method: "POST",
+    headers: {
+      "X-RapidAPI-Key": "581de7aa05mshd7735e7a675a7dbp1fb4f0jsnb36c14cd02e8",
+      "X-RapidAPI-Host": "sameer-kumar-aztro-v1.p.rapidapi.com",
+    },
+  };
   fetch(
-    `http://sandipbgt.com/theastrologer/api/horoscope/${signsSelectValue}/${daySelectValue}`
+    `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${signsSelectValue}&day=${daySelectValue}`,
+    options
   )
     .then((response) => response.json())
     .then((data) => {
       output.innerHTML = `
-<p>Sign: ${data.sunsign}</p>
-<p>Mood: ${data.meta.mood}</p>
-<p>Horoscope: ${data.horoscope}</p>`;
+<p>Date range: ${data.date_range}</p>
+<p>Current date: ${data.current_date}</p>
+<p>Compatibility: ${data.compatibility}</p>
+<p>Mood: ${data.mood}</p>
+<p>Color: ${data.color}</p>
+<p>Lucky number: ${data.lucky_number}</p>
+<p>Lucky time: ${data.lucky_time}</p>
+<p>Horoscope: ${data.description}</p>`;
     });
 });
